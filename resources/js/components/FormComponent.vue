66c24fcf87e23aabce643bbfa5a8251e9da5b823
Orest.L <orest.b2b@gmail.com>
2023-02-21 20:54:47 +0200

    prepare form

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <form>
+        <div class="mb-3">
+            <label for="Email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="Email" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="Password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="Password">
+        </div>
+        <div class="mb-3">
+            <label for="Password repeat" class="form-label">Password repeat</label>
+            <input type="password" class="form-control" id="Password repeat">
+        </div>
+        <div class="mb-3 input-group">
+            <span class="input-group-text">JSON Request</span>
+            <textarea class="form-control"></textarea>
+        </div>
+        <div class="mb-3 input-group">
+            <span class="input-group-text">Response</span>
+            <textarea class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
